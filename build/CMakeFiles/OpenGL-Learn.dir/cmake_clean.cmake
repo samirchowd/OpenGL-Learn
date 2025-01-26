04c2d6be269720c00b3eb4f3321dc0bf25fa6903
Samir Chowdhury <samirchowd@gmail.com>
2025-01-25 18:00:16 -0800

    Adding cmake and window hello world

---
+++ build/CMakeFiles/OpenGL-Learn.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGL-Learn.dir/src/window.cpp.o"
+  "CMakeFiles/OpenGL-Learn.dir/src/window.cpp.o.d"
+  "CMakeFiles/OpenGL-Learn.dir/usr/local/src/glad.c.o"
+  "CMakeFiles/OpenGL-Learn.dir/usr/local/src/glad.c.o.d"
+  "OpenGL-Learn"
+  "OpenGL-Learn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/OpenGL-Learn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
