fefe9da19767f0b65592e03729fe89870fb31847
Samir Chowdhury <samirchowd@gmail.com>
2025-04-19 18:52:41 -0700

    Moving to meson build and finished up triangle tutorial

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,7 @@ void processInput(GLFWwindow *window) {
   }
 }
 
-int main() {
+int main(int argc, char *argv[]) {
   // ==========================================================================
   //                            Window Setup
   // ==========================================================================
@@ -91,70 +91,5 @@ int main() {
   glDeleteShader(vertexShader);
   glDeleteShader(fragmentShader);
 
-  // ==========================================================================
-  //                            Vertex
-  // ==========================================================================
-  // Vertex Data
-  GLfloat vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f,
-                        0.0f,  0.0f,  0.5f, 0.0f};
-
-  // Generate vertex buffer and VAO (configures how shader reads buffer)
-  GLuint VBO, VAO;
-  glGenVertexArrays(1, &VAO);
-  glGenBuffers(1, &VBO);
-
-  // Bind VAO first
-  glBindVertexArray(VAO);
-
-  // Bind buffer and then send vertex data to the buffer
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-  // Tell OpenGL how to read buffer data, VAO stores this information
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-  glEnableVertexAttribArray(0);
-
-  // note that this is allowed, the call to glVertexAttribPointer registered VBO
-  // as the vertex attribute's bound vertex buffer object so afterwards we can
-  // safely unbind
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-  // You can unbind the VAO afterwards so other VAO calls won't accidentally
-  // modify this VAO, but this rarely happens. Modifying other VAOs requires a
-  // call to glBindVertexArray anyways so we generally don't unbind VAOs (nor
-  // VBOs) when it's not directly necessary.
-  glBindVertexArray(0);
-
-  glUseProgram(shaderProgram);
-
-  // ==========================================================================
-  //                            Render
-  // ==========================================================================
-  glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-  while (!glfwWindowShouldClose(window)) {
-    // input
-    // -----
-    processInput(window);
-
-    // render
-    // ------
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    // draw our first triangle
-    glBindVertexArray(
-        VAO); // seeing as we only have a single VAO there's no need to bind it
-              // every time, but we'll do so to keep things a bit more organized
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-    // glBindVertexArray(0); // no need to unbind it every time
-
-    // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved
-    // etc.)
-    // -------------------------------------------------------------------------------
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-  }
-  // Terminate the context and return
-  glfwTerminate();
   return 0;
 }
