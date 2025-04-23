410df23764b7b2a4aa47ea233547d8f33507a8de
Samir Chowdhury <samirchowd@gmail.com>
2025-04-22 19:09:48 -0700

    Finished two triangle exercise

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -13,12 +13,19 @@ const char *vertexShaderSource =
     "{\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
     "}\0";
-const char *fragmentShaderSource =
+const char *fragmentShaderSourceOrange =
     "#version 330 core\n"
     "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
     "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+const char *fragmentShaderSourceYellow =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(0.5f, 1.0f, 0.2f, 1.0f);\n"
     "}\n\0";
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
@@ -63,6 +70,8 @@ int main() {
 
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+  glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
   // ==========================================================================
   //                            Shader Program
   // ==========================================================================
@@ -72,36 +81,61 @@ int main() {
   glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
   glCompileShader(vertexShader);
 
-  // Fragment Shader
-  unsigned int fragmentShader;
-  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-  glCompileShader(fragmentShader);
+  // Orange Fragment Shader
+  unsigned int fragmentShaderOrange;
+  fragmentShaderOrange = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShaderOrange, 1, &fragmentShaderSourceOrange, NULL);
+  glCompileShader(fragmentShaderOrange);
 
-  // Create shader program
-  unsigned int shaderProgram;
-  shaderProgram = glCreateProgram();
+  // Yellow Fragment Shader
+  unsigned int fragmentShaderYellow;
+  fragmentShaderYellow = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShaderYellow, 1, &fragmentShaderSourceYellow, NULL);
+  glCompileShader(fragmentShaderYellow);
+
+  // Create orange shader program
+  unsigned int shaderProgramOrange;
+  shaderProgramOrange = glCreateProgram();
 
   // Attached compiled shaders to the program and link them
-  glAttachShader(shaderProgram, vertexShader);
-  glAttachShader(shaderProgram, fragmentShader);
-  glLinkProgram(shaderProgram);
+  glAttachShader(shaderProgramOrange, vertexShader);
+  glAttachShader(shaderProgramOrange, fragmentShaderOrange);
+  glLinkProgram(shaderProgramOrange);
+
+  // Create yellow shader program
+  unsigned int shaderProgramYellow;
+  shaderProgramYellow = glCreateProgram();
+
+  glAttachShader(shaderProgramYellow, vertexShader);
+  glAttachShader(shaderProgramYellow, fragmentShaderYellow);
+  glLinkProgram(shaderProgramYellow);
 
   // Delete shader programs after they've been linked
   glDeleteShader(vertexShader);
-  glDeleteShader(fragmentShader);
+  glDeleteShader(fragmentShaderOrange);
+  glDeleteShader(fragmentShaderYellow);
 
   // ==========================================================================
   //                            Vertex
   // ==========================================================================
   // Vertex Data
-  GLfloat vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f,
-                        0.0f,  0.0f,  0.5f, 0.0f};
+  float vertices[] = {
+      0.5f,  0.5f,  0.0f, // top right
+      0.5f,  -0.5f, 0.0f, // bottom right
+      -0.5f, -0.5f, 0.0f, // bottom left
+      -0.5f, 0.5f,  0.0f  // top left
+  };
+  unsigned int indices[] = {
+      // note that we start from 0!
+      0, 1, 3, // first triangle
+      1, 2, 3  // second triangle
+  };
 
   // Generate vertex buffer and VAO (configures how shader reads buffer)
-  GLuint VBO, VAO;
+  GLuint VBO, VAO, EBO;
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
 
   // Bind VAO first
   glBindVertexArray(VAO);
@@ -109,6 +143,11 @@ int main() {
   // Bind buffer and then send vertex data to the buffer
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  // Bind buffer and send element data to the buffer
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
 
   // Tell OpenGL how to read buffer data, VAO stores this information
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
@@ -119,38 +158,26 @@ int main() {
   // safely unbind
   glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-  // You can unbind the VAO afterwards so other VAO calls won't accidentally
-  // modify this VAO, but this rarely happens. Modifying other VAOs requires a
-  // call to glBindVertexArray anyways so we generally don't unbind VAOs (nor
-  // VBOs) when it's not directly necessary.
-  glBindVertexArray(0);
-
-  glUseProgram(shaderProgram);
+  glBindVertexArray(VAO);
 
   // ==========================================================================
   //                            Render
   // ==========================================================================
   glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
   while (!glfwWindowShouldClose(window)) {
-    // input
-    // -----
     processInput(window);
 
-    // render
-    // ------
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    // Clear screen
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // draw our first triangle
-    glBindVertexArray(
-        VAO); // seeing as we only have a single VAO there's no need to bind it
-              // every time, but we'll do so to keep things a bit more organized
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-    // glBindVertexArray(0); // no need to unbind it every time
+    glUseProgram(shaderProgramOrange);
+    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, NULL);
 
-    // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved
-    // etc.)
-    // -------------------------------------------------------------------------------
+    GLvoid *offset = (GLvoid *)(3 * sizeof(unsigned int));
+    glUseProgram(shaderProgramYellow);
+    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, offset);
+
+    // Handle events
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
