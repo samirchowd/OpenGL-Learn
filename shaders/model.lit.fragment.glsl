2d80d7e650a9ef4c439e8c2e21aacb4b1c31a12e
Samir Chowdhury <samirchowd@gmail.com>
2026-04-26 17:17:28 -0400

    Integrating lighting into model loading

---
+++ shaders/model.lit.fragment.glsl
@@ -0,0 +1,117 @@
+#version 330 core
+
+uniform sampler2D texture_diffuse1;
+uniform sampler2D texture_specular1;
+uniform float shininess;
+
+struct DirLight {
+    vec3 direction;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+uniform DirLight dirLight;
+
+struct PointLight {
+    vec3 position;
+    float constant;
+    float linear;
+    float quadratic;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+#define NR_POINT_LIGHTS 4
+uniform PointLight pointLights[NR_POINT_LIGHTS];
+
+struct SpotLight {
+    vec3 spotDir;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float phi;
+    float phiOuter;
+    bool enabled;
+};
+uniform SpotLight spotLight;
+
+uniform vec3 viewPos;
+
+in vec2 TexCoords;
+in vec3 Normal;
+in vec3 FragPos;
+
+out vec4 FragColor;
+
+vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir);
+vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
+vec3 CalcSpotLight(SpotLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
+
+void main()
+{
+    vec3 norm = normalize(Normal);
+    vec3 viewDir = normalize(viewPos - FragPos);
+
+    vec3 result = CalcDirLight(dirLight, norm, viewDir);
+    for (int i = 0; i < NR_POINT_LIGHTS; i++)
+        result += CalcPointLight(pointLights[i], norm, FragPos, viewDir);
+    if (spotLight.enabled)
+        result += CalcSpotLight(spotLight, norm, FragPos, viewDir);
+
+    FragColor = vec4(result, 1.0);
+}
+
+vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir)
+{
+    vec3 lightDir = normalize(-light.direction);
+    float diff = max(dot(normal, lightDir), 0.0);
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), shininess);
+    vec3 ambient  = light.ambient  * vec3(texture(texture_diffuse1, TexCoords));
+    vec3 diffuse  = light.diffuse  * diff * vec3(texture(texture_diffuse1, TexCoords));
+    vec3 specular = light.specular * spec * vec3(texture(texture_specular1, TexCoords));
+    return (ambient + diffuse + specular);
+}
+
+vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
+{
+    vec3 lightDir = normalize(light.position - fragPos);
+    float diff = max(dot(normal, lightDir), 0.0);
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), shininess);
+    float distance    = length(light.position - fragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance +
+                        light.quadratic * (distance * distance));
+    vec3 ambient  = light.ambient  * vec3(texture(texture_diffuse1, TexCoords));
+    vec3 diffuse  = light.diffuse  * diff * vec3(texture(texture_diffuse1, TexCoords));
+    vec3 specular = light.specular * spec * vec3(texture(texture_specular1, TexCoords));
+    ambient  *= attenuation;
+    diffuse  *= attenuation;
+    specular *= attenuation;
+    return (ambient + diffuse + specular);
+}
+
+vec3 CalcSpotLight(SpotLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
+{
+    float theta = dot(viewDir, normalize(-light.spotDir));
+
+    vec3 ambient  = vec3(0.0);
+    vec3 diffuse  = vec3(0.0);
+    vec3 specular = vec3(0.0);
+
+    if (theta > light.phiOuter) {
+        float diff = max(dot(normal, viewDir), 0.0);
+        vec3 reflectDir = reflect(-viewDir, normal);
+        float spec = pow(max(dot(viewDir, reflectDir), 0.0), shininess);
+        ambient  = light.ambient  * vec3(texture(texture_diffuse1, TexCoords));
+        diffuse  = light.diffuse  * diff * vec3(texture(texture_diffuse1, TexCoords));
+        specular = light.specular * spec * vec3(texture(texture_specular1, TexCoords));
+        float epsilon   = light.phi - light.phiOuter;
+        float intensity = clamp((theta - light.phiOuter) / epsilon, 0.0, 1.0);
+        ambient  *= intensity;
+        diffuse  *= intensity;
+        specular *= intensity;
+    }
+
+    return (ambient + diffuse + specular);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <iostream>
+#include <format>
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void mouse_callback(GLFWwindow *window, double xpos, double ypos);
@@ -20,6 +21,14 @@ Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
+bool spotlight = false;
+
+glm::vec3 pointLightPositions[] = {
+    glm::vec3( 0.7f,  0.2f,  2.0f),
+    glm::vec3( 2.3f, -3.3f, -4.0f),
+    glm::vec3(-4.0f,  2.0f, -12.0f),
+    glm::vec3( 0.0f,  0.0f, -3.0f)
+};
 
 int main() {
   glfwInit();
@@ -50,8 +59,64 @@ int main() {
 
   glEnable(GL_DEPTH_TEST);
 
-  Shader shader("shaders/model.vertex.glsl", "shaders/model.fragment.glsl");
+  Shader shader("shaders/model.lit.vertex.glsl", "shaders/model.lit.fragment.glsl");
+  Shader lightShader("shaders/vertex.glsl", "shaders/light.fragment.glsl");
   Model model("models/backpack/backpack.obj");
+
+  float cubeVertices[] = {
+    -0.5f,-0.5f,-0.5f, 0.5f,-0.5f,-0.5f, 0.5f, 0.5f,-0.5f,
+     0.5f, 0.5f,-0.5f,-0.5f, 0.5f,-0.5f,-0.5f,-0.5f,-0.5f,
+    -0.5f,-0.5f, 0.5f, 0.5f,-0.5f, 0.5f, 0.5f, 0.5f, 0.5f,
+     0.5f, 0.5f, 0.5f,-0.5f, 0.5f, 0.5f,-0.5f,-0.5f, 0.5f,
+    -0.5f, 0.5f, 0.5f,-0.5f, 0.5f,-0.5f,-0.5f,-0.5f,-0.5f,
+    -0.5f,-0.5f,-0.5f,-0.5f,-0.5f, 0.5f,-0.5f, 0.5f, 0.5f,
+     0.5f, 0.5f, 0.5f, 0.5f, 0.5f,-0.5f, 0.5f,-0.5f,-0.5f,
+     0.5f,-0.5f,-0.5f, 0.5f,-0.5f, 0.5f, 0.5f, 0.5f, 0.5f,
+    -0.5f,-0.5f,-0.5f, 0.5f,-0.5f,-0.5f, 0.5f,-0.5f, 0.5f,
+     0.5f,-0.5f, 0.5f,-0.5f,-0.5f, 0.5f,-0.5f,-0.5f,-0.5f,
+    -0.5f, 0.5f,-0.5f, 0.5f, 0.5f,-0.5f, 0.5f, 0.5f, 0.5f,
+     0.5f, 0.5f, 0.5f,-0.5f, 0.5f, 0.5f,-0.5f, 0.5f,-0.5f,
+  };
+
+  unsigned int lightVAO, lightVBO;
+  glGenVertexArrays(1, &lightVAO);
+  glGenBuffers(1, &lightVBO);
+  glBindVertexArray(lightVAO);
+  glBindBuffer(GL_ARRAY_BUFFER, lightVBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+  glBindVertexArray(0);
+
+  shader.use();
+
+  // material
+  shader.setFloat("shininess", 32.0f);
+
+  // directional light
+  shader.setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
+  shader.setVec3("dirLight.ambient",    0.05f, 0.05f, 0.05f);
+  shader.setVec3("dirLight.diffuse",    0.4f,  0.4f,  0.4f);
+  shader.setVec3("dirLight.specular",   0.5f,  0.5f,  0.5f);
+
+  // point lights
+  for (int i = 0; i < 4; i++) {
+    shader.setVec3(std::format("pointLights[{}].position",  i), pointLightPositions[i]);
+    shader.setVec3(std::format("pointLights[{}].ambient",   i), 0.05f, 0.05f, 0.05f);
+    shader.setVec3(std::format("pointLights[{}].diffuse",   i), 0.8f,  0.8f,  0.8f);
+    shader.setVec3(std::format("pointLights[{}].specular",  i), 1.0f,  1.0f,  1.0f);
+    shader.setFloat(std::format("pointLights[{}].constant",  i), 1.0f);
+    shader.setFloat(std::format("pointLights[{}].linear",    i), 0.09f);
+    shader.setFloat(std::format("pointLights[{}].quadratic", i), 0.032f);
+  }
+
+  // spotlight
+  shader.setFloat("spotLight.phi",      glm::cos(glm::radians(12.5f)));
+  shader.setFloat("spotLight.phiOuter", glm::cos(glm::radians(25.0f)));
+  shader.setVec3("spotLight.ambient",   0.0f, 0.0f, 0.0f);
+  shader.setVec3("spotLight.diffuse",   1.0f, 1.0f, 1.0f);
+  shader.setVec3("spotLight.specular",  1.0f, 1.0f, 1.0f);
+  shader.setBool("spotLight.enabled",   spotlight);
 
   float deltaTime = 0.0f;
   float lastFrame = 0.0f;
@@ -68,6 +133,12 @@ int main() {
 
     shader.use();
 
+    shader.setVec3("viewPos", camera.Position);
+
+    // update spotlight direction and toggle
+    shader.setVec3("spotLight.spotDir", camera.Front);
+    shader.setBool("spotLight.enabled", spotlight);
+
     glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom),
                                             (float)SCR_WIDTH / (float)SCR_HEIGHT,
                                             0.1f, 100.0f);
@@ -81,6 +152,20 @@ int main() {
     shader.setMat4("model", modelMat);
 
     model.Draw(shader);
+
+    // render light cubes
+    lightShader.use();
+    lightShader.setMat4("view", view);
+    lightShader.setMat4("projection", projection);
+    glBindVertexArray(lightVAO);
+    for (unsigned int i = 0; i < 4; i++) {
+      glm::mat4 lightMat = glm::mat4(1.0f);
+      lightMat = glm::translate(lightMat, pointLightPositions[i]);
+      lightMat = glm::scale(lightMat, glm::vec3(0.2f));
+      lightShader.setMat4("model", lightMat);
+      glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
+    glBindVertexArray(0);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
@@ -102,6 +187,14 @@ void processInput(GLFWwindow *window, float deltaTime) {
     camera.ProcessKeyboard(LEFT, deltaTime);
   if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
     camera.ProcessKeyboard(RIGHT, deltaTime);
+
+  static bool fKeyPressed = false;
+  if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS && !fKeyPressed) {
+    fKeyPressed = true;
+    spotlight = !spotlight;
+  }
+  if (glfwGetKey(window, GLFW_KEY_F) == GLFW_RELEASE)
+    fKeyPressed = false;
 }
 
 void mouse_callback(GLFWwindow *window, double xpos, double ypos) {
